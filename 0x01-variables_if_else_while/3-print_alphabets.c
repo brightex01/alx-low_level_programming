ea59900043f1df0c721fc69190537fa002554fd3
brightex01 <brightakpaka1@gmail.com>
2022-03-12 00:38:23 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Orint alphabet in lowercase and then in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	char ALP[26] = "ABCDEFGHIJKLMOPQRSTUVWXYZ";
+	int i, l;
+
+	for (i = 0; i < 26; i++)
+	for (l = 0; l < 26; l++)
+	{
+		putchar(alp[i])
+		putchar(ALP[L]);
+	}
+	putchar('\n');
+	return (0);
+}
+
