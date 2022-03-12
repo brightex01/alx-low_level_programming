498ccfd1ba2a6db9aa2bc6a219d64871eca9265d
brightex01 <brightakpaka1@gmail.com>
2022-03-12 01:00:59 -0800

    print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,10 @@ int main(void)
 	char alp[24] = "abcdfghijklmnoprstuvwxyz";
 	int i;
 
-	for (i = 0, i < 24; i++)
+	for (i = 0; i < 24; i++)
 	{
 		putchar(alp[i]);
 	}
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
