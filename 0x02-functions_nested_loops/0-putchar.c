59e15e939af0fc479e175586b7388a79dc4c0015
brightex01 <brightakpaka1@gmail.com>
2022-03-15 15:01:45 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char(_);
+	char(p);
+	char(u);
+	char(t);
+	char(c);
+	char(h);
+	char(a);
+	char(r);
+}
+_putchar('\n');
+return (0);
