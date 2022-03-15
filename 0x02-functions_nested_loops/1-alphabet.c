f8928fdad01d84e2300a5e1b2ac3ec83a97435cc
brightex01 <brightakpaka1@gmail.com>
2022-03-15 15:52:32 -0700

    alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints alphabets in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(alp[i]);
+	}
+	_putchar('\n');
+	return (0);
+
+
+}
