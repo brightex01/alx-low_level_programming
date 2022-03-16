216b3d821b509f750a46b026aa798b35d098cad9
brightex01 <brightakpaka1@gmail.com>
2022-03-15 17:15:24 -0700

    print alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i, l;
+
+	for (i = 0; i < 26; i++)
+	{
+		_putchar(alp[26]);
+	}
+	for (l = i * *10)
+	{
+		_putchar(('l = i * *10\n'));
+	}
+}
