9762528984c692211b9a8be1584d90a78774683f
brightex01 <brightakpaka1@gmail.com>
2022-03-17 23:48:42 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers, from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int i;
+	char num[10] = "0123456789";
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(num[10]);
+	}
+	_putchar('\n');
+	return (0);
+}
