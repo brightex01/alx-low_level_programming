d361638daefa82af8ebf18bd689ac90a6e2f6404
brightex01 <brightakpaka1@gmail.com>
2022-03-17 23:23:40 -0700

    nul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	mul = int a * int b;
+
+	printf("Result: %d\n", mul);
+	return (0);
+}
