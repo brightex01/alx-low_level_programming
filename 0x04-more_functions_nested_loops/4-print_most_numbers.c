e5f40cd898e72c9e8886ff6a2119a8606f46e2b7
brightex01 <brightakpaka1@gmail.com>
2022-03-18 00:33:01 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Function
+ *
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if ((n != 2) && (n != 4))
+			_putchar(n + '0');
+	}
+	_putchar('\n');
+}
